d23737268094691e21f483858760eedf7368a64a
AxAd <asadhusn007@gmail.com>
2024-12-08 14:10:37 +0500

    list, chat, details components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import List from "./components/list/List";
+import Chat from "./components/chat/Chat";
+import Details from "./components/details/Details";
+
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="container">
+      <List />
+      <Chat />
+      <Details />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/list/List.jsx
@@ -0,0 +1,5 @@
+import "./list.css";
+
+export default function List() {
+  return <div className="list">List</div>;
+}
