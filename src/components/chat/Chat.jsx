ccace4c9caee9908032a2061d1ec648bd90d6199
AxAd <asadhusn007@gmail.com>
2024-12-09 00:22:25 +0500

    completed chat section

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,5 +1,79 @@
+import { useState } from "react";
 import "./chat.css";
 
 export default function Chat() {
-  return <div className="chat">Chat</div>;
+  const [text, setText] = useState("");
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="avatar.png" alt="avatar" />
+          <div className="texts">
+            <span>John Doe</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="phone.png" alt="phone" />
+          <img src="video.png" alt="video" />
+          <img src="info.png" alt="info" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message">
+          <img src="avatar.png" alt="avatar" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem
+              architecto earum a sed dolorum neque doloribus quod excepturi sunt
+              assumenda quaerat, id iure in dicta, ad quam fuga cupiditate
+              dolores.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="wallpaper.png" alt="image" />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem
+              architecto earum a sed dolorum neque doloribus quod excepturi sunt
+              assumenda quaerat, id iure in dicta, ad quam fuga cupiditate
+              dolores.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="avatar.png" alt="avatar" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem
+              architecto earum a sed dolorum neque doloribus quod excepturi sunt
+              assumenda quaerat, id iure in dicta, ad quam fuga cupiditate
+              dolores.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="img.png" alt="img" />
+          <img src="camera.png" alt="camera" />
+          <img src="mic.png" alt="microphone" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <img src="emoji.png" alt="emoji" />
+        {/* <div className="emoji">
+        </div> */}
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
 }
